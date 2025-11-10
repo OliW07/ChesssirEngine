cae829e1c0f6cd69ce53ef13f45198288c936a21
Oliver White <oliverjwhite07@gmai.com>
2025-11-11 12:50:27 +1300

    Refactoring utilities

--- main.cpp
+++ main.cpp
@@ -243,32 +243,6 @@ void visualiseGraphicBoard(BoardState state){
               Helper functions
 --------------------------------------------*/
 
-int convertNotationToInt(const std::string &notation){
-    
-    int file = static_cast<int>(notation[0] - 'a');
-    int rank = static_cast<int>(notation[1] - '0');
-
-    if((file > 7) || (file < 0) || (rank > 8) || (rank < 1)){
-        throw std::runtime_error("Notation "+notation+"is out of range ");
-    }
-
-    return (file + (rank-1)*8);
-
-}
-
-int convertLocationToRows(const int location){
-    if(location < 0 || location > 64){
-        throw std::runtime_error("Location is out of range");
-    }
-    return location/8;
-}
-
-int convertLocationToColumns(const int location){
-    if(location < 0 || location > 63){
-        throw std::runtime_error("Location "+std::to_string(location)+" is out of range");
-    }
-    return location%8;
-}
 
 void updatePieceBitBoards(BoardState &state){
 
