2d925a2ffcb5a8e2e1e93563deadb15f1a42c7ba
Oliver White <oliverjwhite07@gmai.com>
2025-11-14 09:33:15 +1300

    Inverting L/R visualisation of graphic bitboard

--- tools/debug.cpp
+++ tools/debug.cpp
@@ -33,53 +33,57 @@ void visualiseGraphicBoard(BoardState state){
 
     std::cout << "\n";
 
-    for(int i = 0; i < 64; i++){
+    for(int rows = 7; rows >= 0; rows--){
+        for(int columns = 0; columns < 8; columns++){
 
-        //At each location, check all possible piece and if present add to array
+            int pos = 8*rows+columns;
+        
+            //At each location, check all possible piece and if present add to array
 
-        bool whitePawn = state.whitePawnBitBoard & (1ULL << 63),
-            whiteBishop = state.whiteBishopBitBoard & (1ULL << 63),
-            whiteKnight = state.whiteKnightBitBoard & (1ULL << 63),
-            whiteRook = state.whiteRookBitBoard & (1ULL << 63),
-            whiteKing = state.whiteKingBitBoard & (1ULL << 63),
-            whiteQueen = state.whiteQueenBitBoard & (1ULL << 63),
-            blackPawn = state.blackPawnBitBoard & (1ULL << 63),
-            blackBishop = state.blackBishopBitBoard & (1ULL << 63),
-            blackKnight = state.blackKnightBitBoard & (1ULL << 63),
-            blackRook = state.blackRookBitBoard & (1ULL << 63),
-            blackKing = state.blackKingBitBoard & (1ULL << 63),
-            blackQueen = state.blackQueenBitBoard & (1ULL << 63);
+            bool whitePawn = state.whitePawnBitBoard & (1ULL << pos),
+                whiteBishop = state.whiteBishopBitBoard & (1ULL << pos),
+                whiteKnight = state.whiteKnightBitBoard & (1ULL << pos),
+                whiteRook = state.whiteRookBitBoard & (1ULL << pos),
+                whiteKing = state.whiteKingBitBoard & (1ULL << pos),
+                whiteQueen = state.whiteQueenBitBoard & (1ULL << pos),
+                blackPawn = state.blackPawnBitBoard & (1ULL << pos),
+                blackBishop = state.blackBishopBitBoard & (1ULL << pos),
+                blackKnight = state.blackKnightBitBoard & (1ULL << pos),
+                blackRook = state.blackRookBitBoard & (1ULL << pos),
+                blackKing = state.blackKingBitBoard & (1ULL << pos),
+                blackQueen = state.blackQueenBitBoard & (1ULL << pos);
 
-        state.whitePawnBitBoard <<= 1;
-        state.whiteBishopBitBoard <<= 1;
-        state.whiteKnightBitBoard <<= 1;
-        state.whiteRookBitBoard <<= 1;
-        state.whiteKingBitBoard <<= 1;
-        state.whiteQueenBitBoard <<= 1;
-        state.blackPawnBitBoard <<= 1;
-        state.blackKnightBitBoard <<= 1;
-        state.blackBishopBitBoard <<= 1;
-        state.blackRookBitBoard <<= 1;
-        state.blackKingBitBoard <<= 1;
-        state.blackQueenBitBoard <<= 1;
+            /*state.whitePawnBitBoard <<= 1;
+            state.whiteBishopBitBoard <<= 1;
+            state.whiteKnightBitBoard <<= 1;
+            state.whiteRookBitBoard <<= 1;
+            state.whiteKingBitBoard <<= 1;
+            state.whiteQueenBitBoard <<= 1;
+            state.blackPawnBitBoard <<= 1;
+            state.blackKnightBitBoard <<= 1;
+            state.blackBishopBitBoard <<= 1;
+            state.blackRookBitBoard <<= 1;
+            state.blackKingBitBoard <<= 1;
+            state.blackQueenBitBoard <<= 1;*/
 
-        if(whitePawn) temp = 'P';
-        else if(whiteBishop) temp = 'B';
-        else if(whiteKnight) temp = 'N';
-        else if(whiteRook) temp = 'R';
-        else if(whiteQueen) temp = 'Q';
-        else if(whiteKing) temp = 'K';
-        else if(blackPawn) temp = 'p';
-        else if(blackKnight) temp = 'n';
-        else if(blackBishop) temp = 'b';
-        else if(blackQueen) temp = 'q';
-        else if(blackKing) temp = 'k';
-        else if(blackRook) temp = 'r';
-        else temp = '-';
+            if(whitePawn) temp = 'P';
+            else if(whiteBishop) temp = 'B';
+            else if(whiteKnight) temp = 'N';
+            else if(whiteRook) temp = 'R';
+            else if(whiteQueen) temp = 'Q';
+            else if(whiteKing) temp = 'K';
+            else if(blackPawn) temp = 'p';
+            else if(blackKnight) temp = 'n';
+            else if(blackBishop) temp = 'b';
+            else if(blackQueen) temp = 'q';
+            else if(blackKing) temp = 'k';
+            else if(blackRook) temp = 'r';
+            else temp = '-';
 
-        std::cout << temp << "   ";
+            std::cout << temp << "   ";
+        }
 
-        if((i+1)%8==0) std::cout << "\n\n";
+        std::cout << "\n\n";
 
     }
 
