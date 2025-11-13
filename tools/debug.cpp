7033059954b7b02a949c8fe7d1a87c114f0da4b1
Oliver White <oliverjwhite07@gmai.com>
2025-11-13 20:18:11 +1300

    Flipped the board for white perspective visualisation

--- tools/debug.cpp
+++ tools/debug.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <bitset>
 #include <cstdint>
-
+#include <algorithm>
 #include "utils/Types.h"
 #include "debug.h"
 
@@ -14,6 +14,8 @@ void visualiseBitBoard(const uint64_t &bitBoard){
 
     for(int i = 0; i < 8; i++){
         std::string ss = bitString.substr(8*i, 8);
+
+        std::reverse(ss.begin(),ss.end());
 
         for(char c : ss){
            std::cout << c << " ";
@@ -35,31 +37,31 @@ void visualiseGraphicBoard(BoardState state){
 
         //At each location, check all possible piece and if present add to array
 
-        bool whitePawn = state.whitePawnBitBoard & 1,
-            whiteBishop = state.whiteBishopBitBoard & 1,
-            whiteKnight = state.whiteKnightBitBoard & 1,
-            whiteRook = state.whiteRookBitBoard & 1,
-            whiteKing = state.whiteKingBitBoard & 1,
-            whiteQueen = state.whiteQueenBitBoard & 1,
-            blackPawn = state.blackPawnBitBoard & 1,
-            blackBishop = state.blackBishopBitBoard & 1,
-            blackKnight = state.blackKnightBitBoard & 1,
-            blackRook = state.blackRookBitBoard & 1,
-            blackKing = state.blackKingBitBoard & 1,
-            blackQueen = state.blackQueenBitBoard & 1;
+        bool whitePawn = state.whitePawnBitBoard & (1ULL << 63),
+            whiteBishop = state.whiteBishopBitBoard & (1ULL << 63),
+            whiteKnight = state.whiteKnightBitBoard & (1ULL << 63),
+            whiteRook = state.whiteRookBitBoard & (1ULL << 63),
+            whiteKing = state.whiteKingBitBoard & (1ULL << 63),
+            whiteQueen = state.whiteQueenBitBoard & (1ULL << 63),
+            blackPawn = state.blackPawnBitBoard & (1ULL << 63),
+            blackBishop = state.blackBishopBitBoard & (1ULL << 63),
+            blackKnight = state.blackKnightBitBoard & (1ULL << 63),
+            blackRook = state.blackRookBitBoard & (1ULL << 63),
+            blackKing = state.blackKingBitBoard & (1ULL << 63),
+            blackQueen = state.blackQueenBitBoard & (1ULL << 63);
 
-        state.whitePawnBitBoard >>= 1;
-        state.whiteBishopBitBoard >>= 1;
-        state.whiteKnightBitBoard >>= 1;
-        state.whiteRookBitBoard >>= 1;
-        state.whiteKingBitBoard >>= 1;
-        state.whiteQueenBitBoard >>= 1;
-        state.blackPawnBitBoard >>= 1;
-        state.blackKnightBitBoard >>= 1;
-        state.blackBishopBitBoard >>= 1;
-        state.blackRookBitBoard >>= 1;
-        state.blackKingBitBoard >>= 1;
-        state.blackQueenBitBoard >>= 1;
+        state.whitePawnBitBoard <<= 1;
+        state.whiteBishopBitBoard <<= 1;
+        state.whiteKnightBitBoard <<= 1;
+        state.whiteRookBitBoard <<= 1;
+        state.whiteKingBitBoard <<= 1;
+        state.whiteQueenBitBoard <<= 1;
+        state.blackPawnBitBoard <<= 1;
+        state.blackKnightBitBoard <<= 1;
+        state.blackBishopBitBoard <<= 1;
+        state.blackRookBitBoard <<= 1;
+        state.blackKingBitBoard <<= 1;
+        state.blackQueenBitBoard <<= 1;
 
         if(whitePawn) temp = 'P';
         else if(whiteBishop) temp = 'B';
