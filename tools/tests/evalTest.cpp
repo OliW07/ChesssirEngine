0ba8daf9d9f9321a04c6b5d9c5f1c3eb4cad9771
Oliver White <oliverjwhite07@gmai.com>
2026-01-28 16:43:09 +1300

    Updated tests for new eval function

--- tools/tests/evalTest.cpp
+++ tools/tests/evalTest.cpp
@@ -65,11 +65,19 @@ INSTANTIATE_TEST_SUITE_P(
     Positions,
     EvalTestFixture,
     ::testing::Values(
-        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "e2a6 c7c5", PieceValues[Bishop]},
-        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "e2a6 c7c5 d5c6", 430},
-        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "e2a6 c7c5 d5c6 a8b8 c6c7 f6e4 c7b8q", 1630},
-        Position{STARTING_FEN,"",0},
-        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -","", 0}
 
+        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -","", 105},
+        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "e2a6 c7c5 d5c6", 520},
+        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "e2a6 c7c5 d5c6 a8b8", 520},
+        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "e2a6 c7c5 d5c6 a8b8 c6c7", 550},
+        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "e2a6 c7c5 d5c6 a8b8 c6c7 f6e4", 420},
+        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "e2a6 c7c5 d5c6 a8b8 c6c7 f6e4 c7b8q", 1660},
+        Position{STARTING_FEN,"",0},
+        Position{"5k2/8/8/8/8/8/P7/2K5 w - - 0 1", "a2a4",PieceValues[Pawn]},
+        Position{"8/5k2/8/1b6/8/2N5/8/4K3 w - - 0 4", "", 0},
+        Position{"8/5k2/8/1b6/8/2N5/8/4K3 w - - 0 4", "c3b5", 325},
+        Position{"5k2/8/8/8/8/8/P7/2K5 w - - 0 1", "a2a4 f8e8 a4a5", PieceValues[Pawn] + 15},
+        Position{"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", "e2a6 c7c5", 425}
+         
     )
 );
